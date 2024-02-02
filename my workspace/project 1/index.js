bf526839c25b5bb08659019c15ed67b1f4d50b33
Dubea <dubea001@gmail.com>
2024-02-02 16:23:26 +0100

    updated code on dictionary 2nd feb

--- my workspace/project 1/index.js
+++ my workspace/project 1/index.js
@@ -1,45 +1,76 @@
 const input = document.querySelector('#input');
 const searchBtn = document.querySelector('#submit');
-const searchedWord = document.querySelector('#word');
-const partOfSpeech = document.querySelector('#parts-of-speech');
-const definitionDiv = document.querySelector('#definition-div');
-const exampleDiv = document.querySelector('#example-div');
-const antonymsParagragh = document.querySelector('#display-div');
-const synonymsParagragh = document.querySelector('#display-div-2');
+const antonymsDiv = document.querySelector('#antonyms-div');
+const synonymsDiv = document.querySelector('#synonyms-div');
 const baseUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/`;
+const wordDiv = document.querySelector('.js-word-div');
+const containerDiv = document.querySelector('.js-container-div');
 
-let html = "";
 
 async function getWord() {
   const data = await fetch(baseUrl + input.value);
   const response = await data.json();
   const result = response[0];
-  const phonetics = result.phonetics;
-  searchedWord.innerText = result.word;
   const resultObject = result.meanings[0];
-  partOfSpeech.innerText = resultObject.partOfSpeech;
+
   const definitions = resultObject.definitions;
-  const antonyms = resultObject.antonyms;
-  const synonyms = resultObject.synonyms;
-  definitionDiv.innerText = definitions[0].definition;
-  console.log(result.meanings[0]);
-  input.value = "";
+  function checkForExample() {
+    const displayExample = document.querySelector('#example');
+    const example = definitions[0].example !== undefined ? definitions[0].example : "";
+    if (example !== "") {
+      displayExample.textContent = `Example: ${example}`;
+    }
+  };
 
-  antonyms.forEach(item => {
-    html += `<p>${item}</p>`;
-    antonymsParagragh.innerHTML = `Antonyms- ${html}`;
+  let generatedHtml = "";
+  generatedHtml = `<h1 id="word">${result.word.toUpperCase()}</h1>
+        <h3 id="parts-of-speech">${resultObject.partOfSpeech}</h3>
+        <h4>${result.phonetic}</h4>
+        <h2 id="definition-div">${definitions[0].definition}</h2>`;
+  wordDiv.innerHTML = generatedHtml;
+  checkForExample();
+
+  const phonetics = result.phonetics;
+  phonetics.forEach(phone => {
+    const myAudio = document.querySelector("#myAudio");
+    myAudio.src = phone.audio;
+    function playAudio() {
+      myAudio.play();
+    }
+    if (phone.audio !== "") {
+      const playBtn = document.querySelector("#playBtn");
+      playBtn.src = "images/playbutton.jpg";
+      playBtn.addEventListener('click', playAudio);
+    } else {
+      return;
+    }
   });
-  synonyms.forEach(item1 => {
-    html += `<p>${item1}</p>`;
-    synonymsParagragh.innerHTML = `Synonyms- ${html}`;
-  })
 
-  if (definitions[0].example) {
-    exampleDiv.innerText = `Example- ${definitions[0].example}`;
-  } else {
-    return;
-  }
-}
+  function removeDuplicate(arrayValue) {
+    const uniqueArray = Array.from(new Set(arrayValue));
+    return uniqueArray;
+  };
+  const synonyms = removeDuplicate(resultObject.synonyms);
+  const antonyms = removeDuplicate(resultObject.antonyms);
+
+  function addItem(itemValue) {
+    let html = "";
+    itemValue.forEach((item) => {
+      html += `<p class="para-el js-para-el">${item}</p>`;
+      if (itemValue === synonyms) {
+        synonymsDiv.innerHTML = `<h5>Synonyms</h5>${html}`;
+      } else if (itemValue === antonyms) {
+        antonymsDiv.innerHTML = `<h5>Antonyms</h5>${html}`;
+      } else {
+        return;
+      }
+    })
+  };
+
+  addItem(synonyms);
+  addItem(antonyms);
+
+};
 
 searchBtn.addEventListener('click', () => {
   if (input.value !== "") {
@@ -47,5 +78,7 @@ searchBtn.addEventListener('click', () => {
   } else {
     return;
   }
+  input.value = "";
+
 });
 
